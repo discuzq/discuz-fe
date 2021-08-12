89c45f920301ffaff921c81311e64711acc77d6d
Sean Luo <luoxinran@gmail.com>
2021-08-12 11:02:02 +0800

    搜索页面名称使用camel模式并且在baselayout中声明

--- common/store/baselayout/store.js
+++ common/store/baselayout/store.js
@@ -32,6 +32,18 @@ class IndexStore {
 
   @observable topicDetail = -1; // 搜索页面list位置 - 话题列表
 
+  @observable videoFullScreenStatus = ''; // 视频全屏播放的状态
+
+  @observable resultTopic = -1; // 搜索页面list位置 - 更多潮流话题
+
+  @observable resultUser = -1; // 搜索页面list位置 - 更多活跃用户
+
+  @observable resultPost = -1; // 搜索页面list位置 - 更多热门内容
+
+  @observable h5SearchResult = -1; // H5搜索结果页面list位置 
+
+  @observable topicDetail = -1; // 搜索页面list位置 - 话题列表
+
 }
 
 export default IndexStore;
