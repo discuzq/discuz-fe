8576d91b03406227b8ff0c71e6cb68176dd645d2
lizhencui <927117360@qq.com>
2021-08-27 18:26:26 +0800

    feat: 用户角色中限制用户权限

--- web/components/thread/ThreadCenterView.jsx
+++ web/components/thread/ThreadCenterView.jsx
@@ -42,6 +42,12 @@ const Index = (props) => {
       onTextItemClick
     } = props
 
+    const {
+      canDownloadAttachment,
+      canViewAttachment,
+      canViewVideo
+    } = props?.data?.ability || {};
+
   // 标题显示37个字符
   const newTitle = useMemo(() => {
     if (title.length > 100) {
@@ -91,6 +97,7 @@ const Index = (props) => {
                     status={videoData.status}
                     onVideoReady={props.onVideoReady}
                     updateViewCount={updateViewCount}
+                    canViewVideo={canViewVideo}
                   />
                 </WrapperView>
 
@@ -130,7 +137,15 @@ const Index = (props) => {
                   onClick={onClick}
               />}
               {audioData && <AudioPlay url={audioData.mediaUrl} isPay={needPay} onPay={onPay} updateViewCount={updateViewCount}/>}
-            {fileData?.length > 0 && <AttachmentView threadId={threadId} attachments={fileData} onPay={onPay} isPay={needPay} updateViewCount={updateViewCount} />}
+            {fileData?.length > 0 && <AttachmentView
+                  threadId={threadId}
+                  attachments={fileData}
+                  onPay={onPay}
+                  isPay={needPay}
+                  updateViewCount={updateViewCount}
+                  canViewAttachment={canViewAttachment}
+                  canDownloadAttachment={canDownloadAttachment}
+              />}
             {/* 投票帖子展示 */}
             {voteData && <VoteDisplay recomputeRowHeights={props.recomputeRowHeights} voteData={voteData} threadId={threadId} />}
 
